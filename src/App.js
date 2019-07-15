253ded72b63eb390adf84e040b42c0d9e09cd5a2
Sven Reiser <sreiser@anynines.com>
2019-07-15 20:13:31 +0200

    adds toggle mode (light / dark)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext } from 'react'
+import { BrowserRouter as Router, Switch } from 'react-router-dom'
+import styled from 'styled-components'
 
-function App() {
+// L A Y O U T
+import AuthLayout from './layouts/AuthLayout'
+import ProjectLayout from './layouts/ProjectLayout'
+import QuestLayout from './layouts/QuestLayout'
+
+// C O M P O N E N T S
+import Login from './components/auth/Login'
+import Signup from './components/auth/Signup'
+import Alert from './components/elements/Alert'
+import Backlog from './components/project/Backlog'
+
+// C O N T E X T
+import { GlobalContext } from './context/GlobalContext'
+
+const App = props => {
+  const { mode } = useContext(GlobalContext)
+
+  const Root = styled.div`
+    --black: ${mode ? 'rgba(44, 44, 44, 1)' : 'rgba(255, 255, 255, 1)'};
+    --black_25: ${mode ? 'rgba(44, 44, 44, .25)' : 'rgba(255, 255, 255, .25)'};
+    --dark: ${mode ? 'rgba(65, 65, 65, 1)' : 'rgba(235, 234, 236, 1)'};
+    --light: ${mode ? 'rgba(235, 234, 236, 1)' : 'rgba(65, 65, 65, 1)'};
+    --light_50: ${mode ? 'rgba(235, 234, 236, 0.5)' : 'rgba(65, 65, 65, 0.5)'};
+    --white: ${mode ? 'rgba(255, 255, 255, 1)' : 'rgba(44, 44, 44, 1)'};
+    --box-shadow: ${mode
+      ? '0 0 3px rgba(44, 44, 44, .25)'
+      : '0 0 3px rgba(255, 255, 255, .25)'};
+    --box-shadow-active: ${mode
+      ? '0 0 10px rgba(44, 44, 44, .25)'
+      : '0 0 10px rgba(255, 255, 255, .25)'};
+  `
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Root>
+      <Router>
+        <Switch>
+          <AuthLayout path='/' exact component={Login} props={props} />
+          <AuthLayout path='/signup' component={Signup} />
+          <ProjectLayout path='/project' component={Backlog} />
+          <QuestLayout exact path='/project/quest' />
+        </Switch>
+        <Alert />
+      </Router>
+    </Root>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/ToggleMode.js
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react'
+import moon from '../assets/icons/moon.svg'
+import sun from '../assets/icons/sun.svg'
+
+import { GlobalContext } from '../context/GlobalContext'
+
+const ToggleMode = () => {
+  const { mode, setMode } = useContext(GlobalContext)
+  let updatedMode = mode
+  const toggleMode = () => {
+    updatedMode = !updatedMode
+    setMode(updatedMode)
+  }
+  return (
+    <div className='toggle-mode' onClick={toggleMode}>
+      <img src={mode ? moon : sun} alt='' className='icon' />
+    </div>
+  )
+}
+
+export default ToggleMode
