ccd862825e52eb40d4c0eb28e1e48d85d769bb0e
Sven Reiser <sreiser@anynines.com>
2019-07-15 20:56:47 +0200

    moves quests and IOTA handling in QuestList component

--- src/components/project/Backlog.js
+++ src/components/project/Backlog.js
@@ -1,73 +1,16 @@
-import React, { useContext, useState } from 'react'
+import React from 'react'
 
-// IOTA
-import Mam from '@iota/mam'
-import { asciiToTrytes } from '@iota/converter'
-import generateSeed from 'iota-generate-seed'
+// C O M P O N E N T S
+import QuestList from '../questList/QuestList'
 
 // C O N T E X T
-import { GlobalContext } from '../../context/GlobalContext'
+// import { GlobalContext } from '../../context/GlobalContext'
 
 const Backlog = () => {
-  const { seed } = useContext(GlobalContext)
-  let mamState = Mam.init('https://nodes.devnet.iota.org:443', seed)
-
-  const [quest, setQuest] = useState({ title: '' })
-
-  const [quests, setQuests] = useState(
-    JSON.parse(localStorage.getItem('quests') || '[]')
-  )
-
-  const createQuest = () => {
-    publishToMAM(quest).then(response => {
-      quest.seed = generateSeed()
-      let updated_quests = [...quests]
-      updated_quests.push(quest)
-      setQuests(updated_quests)
-      localStorage.setItem('quests', JSON.stringify(updated_quests))
-      setQuest({
-        title: ''
-      })
-    })
-  }
-  const onChange = e => {
-    let value = e.target.value
-    setQuest({
-      title: value
-    })
-  }
-
-  const updateMamState = newMamState => (mamState = newMamState)
-
-  // Publish to tangle
-  const publishToMAM = async data => {
-    // Create MAM Payload - STRING OF TRYTES
-    const trytes = asciiToTrytes(JSON.stringify(data))
-    const message = Mam.create(mamState, trytes)
-
-    // Save new mamState
-    updateMamState(message.state)
-
-    // Attach the payload
-    // let x = await Mam.attach(message.payload, message.address, 3, 9)
-
-    return message
-  }
-
-  const questList = quests.map((quest, index) => (
-    <li key={index}>
-      <h5>{quest.title}</h5>
-    </li>
-  ))
-
   return (
     <div>
       <h1>Project goes here - thats for sure</h1>
-      <p>Seed: {seed}</p>
-      <input value={quest.title} onChange={onChange} type='text' />
-      <button onClick={createQuest}>Create</button>
-      <h2>Quests</h2>
-      <ul>{questList}</ul>
+      <QuestList />
     </div>
   )
 }
---
+++ src/components/questList/QuestList.js
@@ -0,0 +1,72 @@
+import React, { useContext, useState } from 'react'
+
+// I O T A
+import Mam from '@iota/mam'
+import { asciiToTrytes } from '@iota/converter'
+import generateSeed from 'iota-generate-seed'
+
+// C O N T E X T
+import { GlobalContext } from '../../context/GlobalContext'
+const QuestList = () => {
+  const { seed } = useContext(GlobalContext)
+  let mamState = Mam.init('https://nodes.devnet.iota.org:443', seed)
+
+  const [quest, setQuest] = useState({ title: '' })
+
+  const [quests, setQuests] = useState(
+    JSON.parse(localStorage.getItem('quests') || '[]')
+  )
+
+  const createQuest = () => {
+    publishToMAM(quest).then(response => {
+      quest.seed = generateSeed()
+      let updated_quests = [...quests]
+      updated_quests.push(quest)
+      setQuests(updated_quests)
+      localStorage.setItem('quests', JSON.stringify(updated_quests))
+      setQuest({
+        title: ''
+      })
+    })
+  }
+  const onChange = e => {
+    let value = e.target.value
+    setQuest({
+      title: value
+    })
+  }
+
+  const updateMamState = newMamState => (mamState = newMamState)
+
+  // Publish to tangle
+  const publishToMAM = async data => {
+    // Create MAM Payload - STRING OF TRYTES
+    const trytes = asciiToTrytes(JSON.stringify(data))
+    const message = Mam.create(mamState, trytes)
+
+    // Save new mamState
+    updateMamState(message.state)
+
+    // Attach the payload
+    // let x = await Mam.attach(message.payload, message.address, 3, 9)
+
+    return message
+  }
+
+  const questList = quests.map((quest, index) => (
+    <li key={index}>
+      <h5>{quest.title}</h5>
+    </li>
+  ))
+  return (
+    <div>
+      <p>Seed: {seed}</p>
+      <input value={quest.title} onChange={onChange} type='text' />
+      <button onClick={createQuest}>Create</button>
+      <h2>Quests</h2>
+      <ul>{questList}</ul>
+    </div>
+  )
+}
+
+export default QuestList
