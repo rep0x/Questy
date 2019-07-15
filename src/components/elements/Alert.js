f641b3dcc0712fa066d6df78bfa62b46dfa16c0b
Sven Reiser <sreiser@anynines.com>
2019-07-15 20:52:16 +0200

    improves alert

--- src/components/elements/Alert.js
+++ src/components/elements/Alert.js
@@ -1,5 +1,7 @@
 import React, { useContext } from 'react'
+import posed from 'react-pose'
 
+// A S S E T S
 import SvgClose from '../../assets/icons/close'
 
 // C O N T E X T
@@ -7,16 +9,28 @@ import { GlobalContext } from '../../context/GlobalContext'
 
 const Alert = ({ type, msg }) => {
   const { alert, setAlert } = useContext(GlobalContext)
+  const removeAlert = () => {
+    setAlert({
+      type: '',
+      isOpen: false
+    })
+  }
+
+  const AnimatedAlert = posed.div({
+    visible: { scale: 1 },
+    hidden: { scale: 0 }
+  })
 
   return (
-    <div
+    <AnimatedAlert
+      pose={alert.isOpen ? 'visible' : 'hidden'}
       className={`alert alert--${alert.type} ${alert.isOpen ? 'active' : ''}`}
     >
       <span>{alert.msg}</span>
-      <div className='close-wrapper'>
+      <div className='close-wrapper' onClick={removeAlert}>
         <SvgClose />
       </div>
-    </div>
+    </AnimatedAlert>
   )
 }
 
