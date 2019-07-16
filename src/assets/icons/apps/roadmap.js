7cee8db14e4da9cf094d8ac2f9d0abe58c5e3ae8
Sven Reiser <sreiser@anynines.com>
2019-07-16 16:31:24 +0200

    adds app navigation for project layout

---
+++ src/assets/icons/apps/roadmap.js
@@ -0,0 +1,76 @@
+import React from 'react'
+
+const roadmap = () => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='20'
+      height='19'
+      viewBox='0 0 20 19'
+    >
+      <g id='Gruppe_39' data-name='Gruppe 39' transform='translate(-44 -327)'>
+        <path
+          id='Rechteck_99'
+          data-name='Rechteck 99'
+          d='M1,0H1A1,1,0,0,1,2,1V18a0,0,0,0,1,0,0H0a0,0,0,0,1,0,0V1A1,1,0,0,1,1,0Z'
+          transform='translate(44 327)'
+          fill='var(--black)'
+        />
+        <path
+          id='Rechteck_100'
+          data-name='Rechteck 100'
+          d='M0,0H19a1,1,0,0,1,1,1V1a1,1,0,0,1-1,1H0A0,0,0,0,1,0,2V0A0,0,0,0,1,0,0Z'
+          transform='translate(44 344)'
+          fill='var(--black)'
+        />
+        <rect
+          id='Rechteck_101'
+          data-name='Rechteck 101'
+          width='12'
+          height='2'
+          rx='1'
+          transform='translate(47 329)'
+          fill='var(--black)'
+        />
+        <rect
+          id='Rechteck_107'
+          data-name='Rechteck 107'
+          width='9'
+          height='2'
+          rx='1'
+          transform='translate(47 341)'
+          fill='var(--black)'
+        />
+        <rect
+          id='Rechteck_103'
+          data-name='Rechteck 103'
+          width='3'
+          height='2'
+          rx='1'
+          transform='translate(51 332)'
+          fill='var(--black)'
+        />
+        <rect
+          id='Rechteck_106'
+          data-name='Rechteck 106'
+          width='8'
+          height='2'
+          rx='1'
+          transform='translate(54 338)'
+          fill='#fff'
+        />
+        <rect
+          id='Rechteck_105'
+          data-name='Rechteck 105'
+          width='8'
+          height='2'
+          rx='1'
+          transform='translate(50 335)'
+          fill='var(--black)'
+        />
+      </g>
+    </svg>
+  )
+}
+
+export default roadmap
--- src/components/project/Backlog.js
+++ src/components/project/Backlog.js
@@ -5,6 +5,14 @@ import QuestList from '../questList/QuestList'
 import Header from './Header'
 import QuestLog from '../questlog/QuestLog'
 
+// A S S E T S
+import SvgDashboard from '../../assets/icons/apps/dashboard'
+import SvgBacklog from '../../assets/icons/apps/backlog'
+import SvgKanban from '../../assets/icons/apps/kanban'
+import SvgRoadmap from '../../assets/icons/apps/roadmap'
+import SvgTeam from '../../assets/icons/apps/team'
+import SvgSettings from '../../assets/icons/apps/settings'
+
 // C O N T E X T
 // import { GlobalContext } from '../../context/GlobalContext'
 
@@ -13,26 +21,26 @@ const Backlog = () => {
     <Fragment>
       <Header />
       <div className='app-container'>
-        <ul className='app-nav box'>
-          <li className='app-nav--item'>
-            <img src='' alt='' />
-          </li>
-          <li className='app-nav--item'>
-            <img src='' alt='' />
-          </li>
-          <li className='app-nav--item'>
-            <img src='' alt='' />
-          </li>
-          <li className='app-nav--item'>
-            <img src='' alt='' />
-          </li>
-          <li className='app-nav--item'>
-            <img src='' alt='' />
-          </li>
-          <li className='app-nav--item'>
-            <img src='' alt='' />
-          </li>
-        </ul>
+        <div className='app-nav box'>
+          <div className='app-nav--item'>
+            <SvgDashboard />
+          </div>
+          <div className='app-nav--item'>
+            <SvgBacklog />
+          </div>
+          <div className='app-nav--item'>
+            <SvgKanban />
+          </div>
+          <div className='app-nav--item'>
+            <SvgRoadmap />
+          </div>
+          <div className='app-nav--item'>
+            <SvgTeam />
+          </div>
+          <div className='app-nav--item'>
+            <SvgSettings />
+          </div>
+        </div>
         <div className='app-main box'>
           <div className='box-header'>
             <span className='title'>Backlog</span>
