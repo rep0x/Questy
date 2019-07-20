798da646e5a7a182b73b1ccad960bba6ebcf9522
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-20 23:51:50 +0000

    Deploy rep0x/Questy to github.com/rep0x/Questy.git:gh-pages

---
+++ precache-manifest.b9bf1effe2257e7b5d8ef1697325fa1d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd2709aadc881fd0d73cb2ba2239b2be",
+    "url": "/Questy/index.html"
+  },
+  {
+    "revision": "0d9ae5434f50e0b4ad40",
+    "url": "/Questy/static/css/main.55ab33e5.chunk.css"
+  },
+  {
+    "revision": "4674aa6c998db0d4c94a",
+    "url": "/Questy/static/js/2.d7e3d2ea.chunk.js"
+  },
+  {
+    "revision": "0d9ae5434f50e0b4ad40",
+    "url": "/Questy/static/js/main.f3233a28.chunk.js"
+  },
+  {
+    "revision": "3cf0406bb3c3f9be02c5",
+    "url": "/Questy/static/js/runtime~main.f3d7b617.js"
+  },
+  {
+    "revision": "5af79037fc9860738f21581b293a8e0d",
+    "url": "/Questy/static/media/bg.5af79037.png"
+  },
+  {
+    "revision": "d194f17895adb90f1312a5c6211607e0",
+    "url": "/Questy/static/media/moon.d194f178.svg"
+  },
+  {
+    "revision": "b518b95cf2185a0f24c66cf864745943",
+    "url": "/Questy/static/media/sun.b518b95c.svg"
+  }
+]);
